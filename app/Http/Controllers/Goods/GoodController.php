8a417ea0de67c359db9d3b8745055c283c11d981
HyunjinHa <hahj123456@gmail.com>
2024-04-03 16:06:32 +0900

    🔨 chore : post, good controller 修正

--- app/Http/Controllers/Goods/GoodController.php
+++ app/Http/Controllers/Goods/GoodController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Goods;
 
 use App\Http\Controllers\Controller;
 use App\Models\Good;
+use App\Models\GoodReview;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
