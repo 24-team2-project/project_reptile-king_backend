ab6937a7b585d5fd20d38f419057f428eaefd612
HyunjinHa <hahj123456@gmail.com>
2024-04-11 14:50:56 +0900

    category controller

--- app/Http/Controllers/Categories/CategoryController.php
+++ app/Http/Controllers/Categories/CategoryController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
-class NoticeController extends Controller
+class CategoryController extends Controller
 {
     /**
      * Display a listing of the resource.
