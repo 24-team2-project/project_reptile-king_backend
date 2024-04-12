a00a3ea1d5c62353413f653c8cd4333ec94a4021
HyunjinHa <hahj123456@gmail.com>
2024-04-12 10:18:36 +0900

    🔨 chore : CategoryController index 修整

--- app/Http/Controllers/Categories/CategoryController.php
+++ app/Http/Controllers/Categories/CategoryController.php
@@ -13,7 +13,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        $categories = Category::get(['name']);
+        $categories = Category::all();
         return response()->json($categories);
     }
 
