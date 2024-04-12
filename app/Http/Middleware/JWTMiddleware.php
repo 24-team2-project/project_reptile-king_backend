526dec402122948538a4faf0cd7b7d53234f489c
tngjs3327 <psheon9714@gmail.com>
2024-04-12 17:49:34 +0900

    🔨 chore : token認証の修正

--- app/Http/Middleware/JWTMiddleware.php
+++ app/Http/Middleware/JWTMiddleware.php
@@ -21,7 +21,7 @@ class JWTMiddleware
     public function handle(Request $request, Closure $next): Response {
         try {
             // 토큰 유효성 확인 및 인증
-            JWTAuth::parseToken()->authenticate(); // 헤더에서 토큰을 파싱 후, 사용자 인증 
+            JWTAuth::parseToken()->authenticate(); // 헤더에서 토큰을 파싱 후, 사용자 인증 후 저장 
 
         } catch (TokenInvalidException $e) {
             return response()->json(['msg' => '유효하지 않은 토큰'], 401);
@@ -32,18 +32,20 @@ class JWTMiddleware
                 // 만료된 토큰 갱신 시도
                 $refreshToken = JWTAuth::refresh();
                 JWTAuth::setToken($refreshToken);
+                JWTAuth::authenticate(); // 명시적으로 사용자 인증
 
                 // 갱신된 토큰으로 다음 요청 진행
                 $response = $next($request);
 
                 // 응답헤더에 갱신된 토큰 포함
-                $response->headers->set('Authorization', 'Bearer '.$refreshToken);
+                $response->headers->set('Refresh-Token', 'Bearer '.$refreshToken);
                 return $response;
                 
             } catch (JWTException $e) {
                 // 갱신 실패 
                 return response()->json([
                     'msg' => '토큰을 재발급 불가. 재발급 기간 만료, 다시 로그인해 보세요.',
+                    'error' => $e->getMessage(),
                 ], 401);
             }
         }  catch(Exception $e) {
