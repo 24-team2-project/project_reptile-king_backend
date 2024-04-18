e6fd741787d1a2b586079ef46af3d2e5968b707f
HyunjinHa <hahj123456@gmail.com>
2024-04-18 11:15:01 +0900

    🔨 chore : GoodController コード変更及び修正

--- app/Http/Controllers/Users/UserController.php
+++ app/Http/Controllers/Users/UserController.php
@@ -53,9 +53,10 @@ class UserController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request)
     {
-        //
+        $user = JWTAuth::user();
+        
     }
 
     /**
