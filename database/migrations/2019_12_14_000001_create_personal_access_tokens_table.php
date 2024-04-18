cb0ad9a68f8c851f2f0e4fe045ec5e6e963fdf9f
HYEONJUN JO <93760720+JOHYEONJUN39@users.noreply.github.com>
2024-04-18 09:27:44 +0900

    Revert "✨ feat: mQTT接続"

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -17,9 +17,9 @@ return new class extends Migration
             $table->string('name');
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
-            $table->timestampTz('last_used_at')->nullable();
-            $table->timestampTz('expires_at')->nullable();
-            $table->timestampsTz();
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamp('expires_at')->nullable();
+            $table->timestamps();
         });
     }
 
